bc5135d5e677490225fa2afbdc95c66ee399dee6
MR-APEN <aleapen82@gmail.com>
2025-02-09 16:41:00 -0600

    feat: se agrego updateStudent y deleteStudent

--- src/student/student.controller.js
+++ src/student/student.controller.js
@@ -1,3 +1,4 @@
+import { hash, verify } from "argon2"
 import Course from "../course/course.model.js"
 import User from "../user/user.model.js"
 
@@ -18,7 +19,7 @@ export const assignStudentToCourse = async (req, res) => {
         if (student.role !== "STUDENT_ROLE") {
             return res.status(403).json({
                 message: "Solo los estudiantes pueden inscribirse en cursos"
-            });
+            })
         }
   
         const course = await Course.findById(courseId);
@@ -38,29 +39,29 @@ export const assignStudentToCourse = async (req, res) => {
         if (studentCourses.length >= 3) {
             return res.status(400).json({
                 message: "No puedes inscribirte en más de 3 cursos"
-            });
+            })
         }
   
-        course.students.push(studentId);
+        course.students.push(studentId)
         await course.save()
   
         return res.status(200).json({
             message: "Te has inscrito en el curso",
-            course,
+            course
         });
     } catch (err) {
         return res.status(500).json({
             message: "Error al inscribirse en el curso",
-            error: err.message,
+            error: err.message
         })
     }
 }
 
 export const getStudentCourses = async (req, res) => {
     try {
-        const { studentId } = req.params;
+        const { studentId } = req.params
       
-        const student = await User.findById(studentId);
+        const student = await User.findById(studentId)
         if(!student){
             return res.status(404).json({
                 success: false,
@@ -71,19 +72,112 @@ export const getStudentCourses = async (req, res) => {
         if (student.role !== "STUDENT_ROLE") {
             return res.status(403).json({
                 message: "Solo los estudiantes pueden acceder a sus cursos"
-            });
+            })
         }
   
-      const courses = await Course.find({students: studentId}).populate("teacher", "name surname");
+      const courses = await Course.find({students: studentId}).populate("teacher", "name surname")
   
       return res.status(200).json({
         message: "Cursos en los que estás inscrito",
-        courses,
-      });
+        courses
+      })
     } catch (err) {
       return res.status(500).json({
         message: "Error al obtener los cursos del estudiante",
-        error: err.message,
-      });
+        error: err.message
+      })
     }
 }
+
+export const updateStudentProfile = async (req, res) => {
+    try {
+        const { studentId } = req.params
+        const data = req.body
+
+        const student = await User.findById(studentId)
+        if(!student){
+            return res.status(404).json({
+                success: false,
+                message: "Usuario no encontrado",
+                error: err.message
+            })
+        }
+        if (student.role !== "STUDENT_ROLE") {
+            return res.status(403).json({
+                message: "Solo los estudiantes pueden acceder a sus cursos"
+            })
+        }
+
+        const updatedStudent = await User.findByIdAndUpdate(studentId,data, { new: true })
+  
+        return res.status(200).json({
+            message: "Perfil de estudiante actualizado correctamente",
+            updatedStudent
+        })
+    } catch (err) {
+        return res.status(500).json({
+            message: "Error al actualizar el perfil",
+            error: err.message
+        })
+    }
+}
+
+export const updateStudentPassword = async (req, res) => {
+    try{
+        const { studentId } = req.params
+        const { newPassword } = req.body
+
+        const student = await User.findById(studentId)
+
+        const matchOldAndNewPassword = await verify(student.password, newPassword)
+
+        if(matchOldAndNewPassword){
+            return res.status(400).json({
+                success: false,
+                message: "La nueva contraseña no puede ser igual a la anterior"
+            })
+        }
+
+        const encryptedPassword = await hash(newPassword)
+
+        await User.findByIdAndUpdate(studentId, {password: encryptedPassword})
+
+        return res.status(200).json({
+            success: true,
+            message: "Contraseña actualizada",
+        })
+
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error al actualizar contraseña",
+            error: err.message
+        })
+    }
+}
+
+export const deleteStudentProfile = async (req, res) => {
+    try {
+        const { studentId } = req.params
+  
+        const student = await User.findById(studentId)
+        if(!student){
+            return res.status(404).json({
+                success: false,
+                message: "Usuario no encontrado",
+                error: err.message
+            })
+        }
+
+        await User.findByIdAndUpdate(studentId, {status: false}, {new: true})
+  
+        return res.status(200).json({
+            message: "Perfil eliminado correctamente"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error al eliminar el perfil",
+            error: error.message
+        })
+    }
+  }
--- src/student/student.routes.js
+++ src/student/student.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { assignStudentToCourse, getStudentCourses } from "./student.controller.js"
+import { assignStudentToCourse, getStudentCourses, updateStudentProfile, deleteStudentProfile, updateStudentPassword } from "./student.controller.js"
 
 const router = Router()
 
 router.post("/assignToCourse", assignStudentToCourse)
 router.get("/studentCourses/:studentId", getStudentCourses)
+router.put("/updateStudent/:studentId" ,updateStudentProfile)
+router.delete("/deleteStudent/:studentId", deleteStudentProfile)
+router.patch("/updatePassword/:studentId", updateStudentPassword)
 
 export default router
